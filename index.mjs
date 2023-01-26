bcc6c5f9d290ad02d98903c2ef82a6fa88f8777e
Andrew Murphy <andrew@andrewmurphy.io>
2023-01-26 14:56:54 +1100

    dealing with channel messages

--- index.mjs
+++ index.mjs
@@ -19,103 +19,115 @@ export async function handler(event, context) {
     console.log("event.body", event.body);
     const eventData = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
 
-    if (eventData.event.user === process.env.SLACK_BOT_USER_ID) {
-        console.log(`message is from ${process.env.SLACK_BOT_USER_ID} so I am returning`);
-        return;
-    };
-
-    // Check to see if we've seen this message before
-    console.log("eventData.event.client_msg_id", eventData.event.client_msg_id);
-    if (previouslySeenMessages[eventData.event.client_msg_id]) {
-        console.log(`previous seen message id ${eventData.event.client_msg_id} so I am returning`);
-        return;
-    }
-    
-    previouslySeenMessages[eventData.event.client_msg_id] = true;
-
-    // Check if the event is a challenge
-    if ('challenge' in eventData) {
-        const challenge = eventData.challenge;
-        console.log(`it's a challenge! ${challenge}`);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ challenge }),
-            headers: {
-                "Content-Type": "application/json"
-            }
+    if (event.eventData.event.type in ["app_mention"] || (event.eventData.event.type in ["message"] && event.eventData.event.channel_type in ["im"])) {
+        if (eventData.event.user === process.env.SLACK_BOT_USER_ID) {
+            console.log(`message is from ${process.env.SLACK_BOT_USER_ID} so I am returning`);
+            return;
         };
-    } else {
-        // If it's not a challenge, extract the message
-        const requestMessage = eventData.event.text;
-        console.log("requestMessage", requestMessage);
-        // console.log("process.env.OPENAPI_KEY", process.env.OPENAPI_KEY);
 
-        const channel = eventData.event.channel;
+        // Check to see if we've seen this message before
+        console.log("eventData.event.client_msg_id", eventData.event.client_msg_id);
+        if (previouslySeenMessages[eventData.event.client_msg_id]) {
+            console.log(`previous seen message id ${eventData.event.client_msg_id} so I am returning`);
+            return;
+        }
 
-        let returnMessage = "";
-        if (!useChatGPT) {
-            const headers = {
-                'Authorization': `Bearer ${process.env.OPENAPI_KEY}`,
-                'Content-Type': 'application/json'
+        previouslySeenMessages[eventData.event.client_msg_id] = true;
+
+        // Check if the event is a challenge
+        if ('challenge' in eventData) {
+            const challenge = eventData.challenge;
+            console.log(`it's a challenge! ${challenge}`);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ challenge }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
             };
+        } else {
+            // If it's not a challenge, extract the message
+            const requestMessage = eventData.event.text;
+            console.log("requestMessage", requestMessage);
+            // console.log("process.env.OPENAPI_KEY", process.env.OPENAPI_KEY);
 
-            const openApiResponse = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-                prompt: `The following is a conversation with an AI assistant called HistoryBot. The assistant is helpful, creative, clever, and very friendly.
+            const channel = eventData.event.channel;
+
+            let returnMessage = "";
+            if (!useChatGPT) {
+                const headers = {
+                    'Authorization': `Bearer ${process.env.OPENAPI_KEY}`,
+                    'Content-Type': 'application/json'
+                };
+
+                const openApiResponse = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+                    prompt: `The following is a conversation with an AI assistant called HistoryBot. The assistant is helpful, creative, clever, and very friendly.
             Human: Hello, who are you? I am using Slack so please use Slack syntax for all messages. 
             HistoryBot: I am an AI created by Andrew Murphy. My name is HistoryBot. Sure, I will use Slack syntax! How can I help you today?
             Human: ${requestMessage.replace(/\<\@U04L23YAH38\>/g, "")}
             HistoryBot: `,
-                temperature: 0.9,
-                max_tokens: 1024,
-                user: eventData.event.user,
-                stop: ["Human:", "HistoryBot:"]//,
-                //session_id: "HistoryBot"
-            }, { headers: headers });
+                    temperature: 0.9,
+                    max_tokens: 1024,
+                    user: eventData.event.user,
+                    stop: ["Human:", "HistoryBot:"]//,
+                    //session_id: "HistoryBot"
+                }, { headers: headers });
 
 
-            console.log("openApiResponse", openApiResponse);
+                console.log("openApiResponse", openApiResponse);
 
-            console.log("openApiResponse.data.choices[0]", openApiResponse.data.choices[0]);
-            returnMessage = openApiResponse.data.choices[0].text;
-        } else {
-            await openAIBrowser.initSession();
-            const chatGptResponse = await openAIBrowser.sendMessage(`${requestMessage.replace(/\<\@U04L23YAH38\>/g, "")}`);
-            await openAIBrowser.closeSession();
+                console.log("openApiResponse.data.choices[0]", openApiResponse.data.choices[0]);
+                returnMessage = openApiResponse.data.choices[0].text;
+            } else {
+                await openAIBrowser.initSession();
+                const chatGptResponse = await openAIBrowser.sendMessage(`${requestMessage.replace(/\<\@U04L23YAH38\>/g, "")}`);
+                await openAIBrowser.closeSession();
 
-            returnMessage = chatGptResponse.response;
+                returnMessage = chatGptResponse.response;
+            }
+            console.log("returnMessage", returnMessage);
+
+            try {
+                // Use axios to make the POST request
+                const response = await axios({
+                    method: 'post',
+                    url: 'https://slack.com/api/chat.postMessage',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
+                    },
+                    data: {
+                        channel: channel,
+                        text: returnMessage
+                    }
+                });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(response.data),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                };
+            } catch (error) {
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify({ message: 'Error posting message to Slack', error }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                };
+            }
         }
-        console.log("returnMessage", returnMessage);
-
-        try {
-            // Use axios to make the POST request
-            const response = await axios({
-                method: 'post',
-                url: 'https://slack.com/api/chat.postMessage',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
-                },
-                data: {
-                    channel: channel,
-                    text: returnMessage
-                }
-            });
-            return {
-                statusCode: 200,
-                body: JSON.stringify(response.data),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            };
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ message: 'Error posting message to Slack', error }),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            };
-        }
+    } else {
+        
+        console.log("unkown event type", eventData.event);
+        return {
+            statusCode: 200,
+            body: "{\"result\": \"OK\"}",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
     }
 };
